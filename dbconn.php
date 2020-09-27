6307157bb28c2ef5fdcd58ac306f8b57570becf2
Dayoung Kim <kimdayoung771@gmail.com>
2020-09-27 22:59:16 +0900

    server hosting

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,10 @@
 <?php
+    // $conn = mysqli_connect("sql105.epizy.com", "epiz_26803955", "s35rVT1Yp0G", "epiz_26803955_boardTable");
     $conn = mysqli_connect("localhost", "root", 1234, "boardTable");
     if(!$conn){
         die("Connection failed: ".mysqli_connect_error());
     }
     
 ?>
+
 
